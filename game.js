89b883bea7130e6773b38d99be0a18c34f4c8f5c
carlsagan <97457166+NoahEspi@users.noreply.github.com>
2022-02-01 03:01:27 +0000

    created enemy class (not working)

--- game.js
+++ game.js
@@ -1,34 +1,63 @@
 // https://keycode.info/
+// https://p5js.org/reference/
+
+
+/*
+IF MOVEMENT WITH ARROW KEYS IS BEING BUGGY, MAKE SURE THE BLACK WINDOW IS AT FULL SIZE (pull down the console until you see white)
+*/
 
 let x = 100;
 let y = 100;
 
+class Enemy {
+  constructor(name, size, speed) {
+    this.name = name;
+    this.size = size;
+    this.speed = speed;
+  }
+
+  spawnEnemy() {
+    console.log(`Spawned ${this.name}`);
+    var randX = Math.round(Math.random() * 500);
+    var randY = Math.round(Math.random() * 500);
+    ellipse(randX, randY, this.size, this.size);
+    randX -= speed
+    randY -= speed
+  }
+}
+
 function setup() {
   createCanvas(500, 500);
-  background('black')
-  fill('white')
+  background('black');
+  fill('white');
+}
+
+function mousePressed() {
+  console.log(`(${mouseX}, ${mouseY})`)
 }
 
 function draw() {
   if (keyIsDown(16) || keyIsDown(13)) {
-    speed = 10
+    speed = 10;
   } else {
-    speed = 5
+    speed = 5;
   }
-  if (keyIsDown(LEFT_ARROW) && (x > 35) || keyIsDown(65) && (x > 35)) {
-    x -= speed
+  if (keyIsDown(LEFT_ARROW) && (x > 34) || keyIsDown(65) && (x > 34)) {
+    x -= speed;
   }
-  if (keyIsDown(RIGHT_ARROW) && (x < 465) || keyIsDown(68) && (x < 465)) {
-    x += speed
+  if (keyIsDown(RIGHT_ARROW) && (x < 464) || keyIsDown(68) && (x < 464)) {
+    x += speed;
   }
-  if (keyIsDown(UP_ARROW) && (y > 35) || keyIsDown(87) && (y > 35)) {
-    y -= speed
+  if (keyIsDown(DOWN_ARROW) && (y < 464) || keyIsDown(83) && (y < 464)) {
+    y += speed;
   }
-  if (keyIsDown(DOWN_ARROW) && (y < 465) || keyIsDown(83) && (y < 465)) {
-    y += speed
+  if (keyIsDown(UP_ARROW) && (y > 34) || keyIsDown(87) && (y > 34)) {
+    y -= speed;
   }
   
+  // spawnEnemy('John', 5, 10);
+
   clear();
-  background('black')
-  ellipse(x, y, 50, 50)
+  background('black');
+  ellipse(x, y, 50, 50);
 }
